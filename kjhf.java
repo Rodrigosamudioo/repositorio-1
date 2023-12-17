c6a61aac9e0bc1b5a8ab47c1e719f85ce97a1299
rodrigo samudio <rodrigosamudio37@gmail.com>
2023-12-17 20:12:31 -0300

    cambios

---
+++ kjhf.java
@@ -0,0 +1,5 @@
+public class kjhf {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ practica2ejercicio2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * practica2ejercicio2
+ */
+public class practica2ejercicio2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2;
+        
+    }
+}
